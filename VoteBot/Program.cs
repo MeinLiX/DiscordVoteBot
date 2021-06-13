3ca768bcd7897900ad849d6066217ed5097ddff7
MeinLiX <meinlix@ukr.net>
2021-06-13 17:53:57 +0300

    Add project files.

---
+++ VoteBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VoteBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
