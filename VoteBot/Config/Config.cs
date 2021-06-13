7c860b5a7934323e5fe7bf45c2ec0d27961fe677
MeinLiX <meinlix@ukr.net>
2021-06-13 23:04:41 +0300

    NEW: basic reaction logic for messages with a vote hashtag.

---
+++ VoteBot/Config/Config.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteBot.Config
+{
+    class Config
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonIgnore]
+        public const string ConfigFileName = "Config.json";
+
+        public static Config Get()
+        {
+            Config cfg;
+            if (!File.Exists(ConfigFileName))
+            {
+                cfg = new()
+                {
+                    Token = "ENTER ME PLEASE"
+                };
+                File.WriteAllText(ConfigFileName, JsonConvert.SerializeObject(cfg, Formatting.Indented));
+            }
+            else
+            {
+                cfg = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigFileName));
+            }
+            return cfg;
+        }
+    }
+}
--- VoteBot/Program.cs
+++ VoteBot/Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace VoteBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args) => await new Program().RunBotAsync();
+
+        private DiscordSocketClient _client;
+
+        public async Task RunBotAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new();
+
+            RegisterPackagesAsync();
+
+            await _client.LoginAsync(TokenType.Bot, Config.Config.Get().Token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public void RegisterPackagesAsync()
+        {
+            _client.Log += Client_Log;
+            _client.ReactionAdded += HandleReactionAddAsync;
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleReactionAddAsync(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            var msg = await channel.GetMessageAsync(message.Id);
+
+            if (!msg.ToString().Contains("#vote"))
+                return;
+
+            if (((await channel.GetUserAsync(reaction.UserId)) as SocketGuildUser).GuildPermissions.Administrator) 
+                return;
+
+            foreach (var msg_reaction in msg.Reactions)
+            {
+                var userReacted = (await msg?.GetReactionUsersAsync(msg_reaction.Key, 1000).FlattenAsync()) as ICollection<IUser>;
+                foreach (var user_reaction in userReacted)
+                {
+                    if (msg_reaction.Key.Name == reaction.Emote.Name && userReacted.Count > 1)
+                        break;
+
+                    if (user_reaction.Id == reaction.UserId)
+                        await msg.RemoveReactionAsync(msg_reaction.Key, reaction.UserId);
+                }
+            }
+
+            return;
         }
     }
 }
